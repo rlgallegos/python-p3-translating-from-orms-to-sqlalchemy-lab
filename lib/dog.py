e664490a0ec75fa34bbdd37da75499cc03e40bcf
Robert <rlgallegos85@gmail.com>
2023-03-26 16:06:10 -0400

    Finished First SQLAlchemy lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,25 +1,28 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    # if __name__ == '__main__':
+        # engine = create_engine('sqlite:///dogs.db')
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def new_from_db(session):
     pass
 
 def get_all(session):
-    pass
+    return session.query(Dog)
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name ).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
--- lib/models.py
+++ lib/models.py
@@ -5,5 +5,16 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+# Schema
+
 class Dog(Base):
-    pass
+    __tablename__ = 'dogs'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    breed = Column(String())
+
+    def __repr__(self):
+        return f"Dog {self.id}:  " \
+            + f"self.name,  " \
+            + f"self.breed,  "
